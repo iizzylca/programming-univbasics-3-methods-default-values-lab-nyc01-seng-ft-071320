4d8b192769bb5a658314d680b6c072377a59004b
Israel Canessa <israellcanessa@gmail.com>
2020-06-28 01:18:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,6 @@
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return meal_choice
 end
 meal_choice("broccoli", "macaroni","roast beef")
